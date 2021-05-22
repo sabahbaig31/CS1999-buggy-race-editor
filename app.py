c0f946ae7863c149fe8c0c74ceb4564ec01cc180
Sabah Baig <sabahbaig@Sabahs-MacBook-Pro.local>
2021-05-22 19:10:02 +0100

    change for 1-ADD

--- app.py
+++ app.py
@@ -28,15 +28,17 @@ def create_buggy():
     elif request.method == 'POST':
         msg=""
         qty_wheels = request.form['qty_wheels']
+        flag_color = request.form['flag_color']
         try:
             with sql.connect(DATABASE_FILE) as con:
                 cur = con.cursor()
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=? WHERE id=?",
-                    (qty_wheels, DEFAULT_BUGGY_ID)
+                    "UPDATE buggies set qty_wheels=?, flag_color=? WHERE id=?",
+                    (qty_wheels, flag_color, DEFAULT_BUGGY_ID)
                 )
                 con.commit()
                 msg = "Record successfully saved"
+                #msg = f"flag_color={flag_color}"
         except:
             con.rollback()
             msg = "error in update operation"
