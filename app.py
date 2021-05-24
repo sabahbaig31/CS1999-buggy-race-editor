054ddc07df31629012d962ccf297fa550f2bf7c5
Sabah Baig <sabahbaig@Sabahs-MacBook-Pro.local>
2021-05-24 18:48:52 +0100

    1-ADD auxiliary power units

--- app.py
+++ app.py
@@ -34,13 +34,14 @@ def create_buggy():
         power_type = request.form['power_type']
         power_units = request.form['power_units']
         aux_power_type = request.form['aux_power_type']
+        aux_power_units = request.form['aux_power_units']
 
         try:
             with sql.connect(DATABASE_FILE) as con:
                 cur = con.cursor()
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=?, flag_color=?, flag_pattern=?, flag_color_secondary=?, power_type=?, power_units=?, aux_power_type=? WHERE id=?",
-                    (qty_wheels, flag_color, flag_pattern, flag_color_secondary, power_type, power_units, aux_power_type, DEFAULT_BUGGY_ID)
+                    "UPDATE buggies set qty_wheels=?, flag_color=?, flag_pattern=?, flag_color_secondary=?, power_type=?, power_units=?, aux_power_type=?, aux_power_units=? WHERE id=?",
+                    (qty_wheels, flag_color, flag_pattern, flag_color_secondary, power_type, power_units, aux_power_type, aux_power_units, DEFAULT_BUGGY_ID)
                 )
                 con.commit()
                 msg = "Record successfully saved"
--- init_db.py
+++ init_db.py
@@ -31,7 +31,8 @@ connection.execute("""
     flag_pattern          VARCHAR(20) DEFAULT plain,
     power_type            VARCHAR(20) DEFAULT petrol,
     power_units           INTEGER DEFAULT 1,
-    aux_power_type        VARCHAR(20)
+    aux_power_type        VARCHAR(20),
+    aux_power_units       INTEGER DEFAULT 0
   )
 
 """)
